37215f0f8b4df689a439d355187de4792ac98cf5
zjw <942961221@qq.com>
2019-04-09 20:23:51 +0800

    新加了一个方法

---
+++ HelloProjet/src/Test3.java
@@ -0,0 +1,14 @@
+/**
+ * @author : zhangJW
+ * @Description : java类作用描述
+ * @CreateDate : 2019/4/9 20:23
+ * @Updat :  更新作者
+ * @UpdateDate : 2019/4/9 20:23
+ * @UpdateRemark : 更新内容注释
+ * @Version : 1.0
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        System.out.println(3333333);
+    }
+}
