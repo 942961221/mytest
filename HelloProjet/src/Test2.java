35795417357d7057acb2fcb8df55fcd9360c60ab
zjw <942961221@qq.com>
2019-04-09 20:11:47 +0800

    新加了一个方法

---
+++ HelloProjet/src/Test2.java
@@ -0,0 +1,15 @@
+/**
+ * @author : zhangJW
+ * @Description : java类作用描述
+ * @CreateDate : 2019/4/9 20:02
+ * @Updat :  更新作者
+ * @UpdateDate : 2019/4/9 20:02
+ * @UpdateRemark : 更新内容注释
+ * @Version : 1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("222222222");
+    }
+    private String age;
+}
